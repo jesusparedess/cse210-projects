6a42da6d1ee4bf41d41d08e91657a00046f50bdf
jesusparedess <jesusparedess@byui.edu>
2024-01-19 23:49:13 -0500

    resume & job

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -5,6 +5,14 @@ public class Job
     public int _startYear;
     public int _endYear;
 
+     public Job(string company, string jobTitle, int startYear, int endYear)
+    {
+        _company = company;
+        _jobTitle = jobTitle;
+        _startYear = startYear;
+        _endYear = endYear;
+    }
+
     public void Display()
 {
     Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,5 +7,12 @@ class Program
         Job job1 = new Job("Microsoft", "Software Engineer", 2019, 2022); 
         Job job2 = new Job("Apple", "Manager", 2022, 2023);
 
+        Resume myResume = new Resume("Allison Rose");
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display(); 
+
+
     }
 }
