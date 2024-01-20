e71c515c4134cede2634432f3a9edd2a0a027c55
jesusparedess <jesusparedess@byui.edu>
2024-01-20 01:27:54 -0500

    change questions

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,9 +7,9 @@ class Program
     {
         "Who was the most interesting person I interacted with today?",
         "What was the best part of my day?",
-        "How did I see the hand of the Lord in my life today?",
+        "Did you excercise and work on your habits today?",
         "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
+        "Could you tell us how you feel right now?"
     };
 
     static void Main()
