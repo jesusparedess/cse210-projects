b7850d7e52d1a404080e5b7c75caf61b043606d0
jesusparedess <jesusparedess@byui.edu>
2024-02-18 16:51:45 -0500

    more orders

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -6,20 +6,32 @@ class Program
     {
         Product product1 = new Product("Laptop", "LP100", 999.99, 1);
         Product product2 = new Product("Mouse", "MS200", 19.99, 2);
+        Product product3 = new Product("Speakers", "S300", 19.99, 4);
 
         // Create addresses and customers
-        Address address1 = new Address("123 Main St", "Anytown", "CA", "USA");
-        Customer customer1 = new Customer("John Doe", address1);
+        Address address1 = new Address("123 Main St", "SanFran", "CA", "USA");
+        Customer customer1 = new Customer("Jesus Paredes", address1);
+        Address address2 = new Address("321 Principal St", "Manhattan", "NY", "USA");
+        Customer customer2 = new Customer("Maria Castro", address2);
 
         // Create orders
         Order order1 = new Order(customer1);
         order1.AddProduct(product1);
         order1.AddProduct(product2);
 
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product2);
+        order2.AddProduct(product3);
+
         // Display packing and shipping labels and total cost for the order
         Console.WriteLine(order1.GetPackingLabel());
         Console.WriteLine(order1.GetShippingLabel());
         Console.WriteLine($"Total cost: ${order1.CalculateTotalCost()}");
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total cost: ${order2.CalculateTotalCost()}");
+
 
         // Repeat the process for another order with different products and customer
         // ...
