0aa7767e1655a96e8a31bc4ca544c6aca6ff8af3
jesusparedess <jesusparedess@byui.edu>
2024-02-19 23:26:10 -0500

    fixing

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -8,13 +8,13 @@ class Program
         Product product2 = new Product("Mouse", "MS200", 19.99, 2);
         Product product3 = new Product("Speakers", "S300", 79.99, 4);
 
-        // Create addresses and customers
+        
         Address address1 = new Address("123 Main St", "SanFran", "CA", "USA");
         Customer customer1 = new Customer("Jesus Paredes", address1);
         Address address2 = new Address("321 Principal St", "Manhattan", "NY", "USA");
         Customer customer2 = new Customer("Maria Castro", address2);
 
-        // Create orders
+        
         Order order1 = new Order(customer1);
         order1.AddProduct(product1);
         order1.AddProduct(product2);
@@ -23,7 +23,7 @@ class Program
         order2.AddProduct(product2);
         order2.AddProduct(product3);
 
-        // Display packing and shipping labels and total cost for the order
+       
         Console.WriteLine(order1.GetPackingLabel());
         Console.WriteLine(order1.GetShippingLabel());
         Console.WriteLine($"Total cost: ${order1.CalculateTotalCost()}");
@@ -33,10 +33,6 @@ class Program
         Console.WriteLine($"Total cost: ${order2.CalculateTotalCost()}");
 
 
-        // Repeat the process for another order with different products and customer
-        // ...
-
-        // Wait for user input to close the console window
         Console.ReadLine();
     }
 }
