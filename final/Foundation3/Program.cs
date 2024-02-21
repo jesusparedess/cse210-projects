571d0c599af8d24ad34927eb153f4a03e34ce3ec
jesusparedess <jesusparedess@byui.edu>
2024-02-21 00:00:23 -0500

    edit

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,11 +4,13 @@ class Program
 {
     public static void Main()
     {
-        Address eventAddress = new Address("123 Event Lane", "Eventville", "EventState", "EventCountry");
+        Address eventAddress1 = new Address("123 Central Park St", "Old York", "YN", "USA");
+        Address eventAddress2 = new Address("456 Freedom Av", "Bos Town", "AM", "USA");
+        Address eventAddress3 = new Address("Main Square", "Blue Beach", "LF", "USA");
 
-        Event lecture = new LectureEvent("Lecture on Event Planning", "Learn about event planning.", DateTime.Today, new TimeSpan(18, 0, 0), eventAddress, "Jane Doe", 100);
-        Event reception = new ReceptionEvent("Annual Reception", "Join us for our annual reception.", DateTime.Today.AddDays(1), new TimeSpan(19, 30, 0), eventAddress, "[email protected]");
-        Event outdoor = new OutdoorEvent("Outdoor Concert", "Enjoy an evening of music.", DateTime.Today.AddDays(2), new TimeSpan(17, 0, 0), eventAddress, "Sunny with a chance of showers");
+        Event lecture = new LectureEvent("Lecture on Event Planning", "Learn about event planning.", DateTime.Today, new TimeSpan(18, 0, 0), eventAddress1, "Jane Doe", 100);
+        Event reception = new ReceptionEvent("Annual Reception", "Join us for our annual reception.", DateTime.Today.AddDays(1), new TimeSpan(19, 30, 0), eventAddress2, "[email protected]");
+        Event outdoor = new OutdoorEvent("Outdoor Concert", "Enjoy an evening of music.", DateTime.Today.AddDays(2), new TimeSpan(17, 0, 0), eventAddress3, "Sunny with a chance of showers");
 
         PrintEventDetails(lecture);
         Console.WriteLine("\n");
@@ -20,7 +22,9 @@ class Program
     private static void PrintEventDetails(Event eventItem)
     {
         Console.WriteLine(eventItem.GetStandardDetails());
+
         Console.WriteLine(eventItem.GetFullDetails());
+
         Console.WriteLine(eventItem.GetShortDescription());
     }
 }
